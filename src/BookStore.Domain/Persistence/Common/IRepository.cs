f5226b0711c6b9c64c944dc18602ea4b62e3a329
Lucas Iglesias <lucasiglesias2704@gmail.com>
2025-10-29 23:34:02 -0300

    feat: more repository impl

--- src/BookStore.Domain/Persistence/Common/IRepository.cs
+++ src/BookStore.Domain/Persistence/Common/IRepository.cs
@@ -2,7 +2,8 @@
 
 namespace BookStore.Domain.Persistence.Common
 {
-    public interface IRepository<TEntity, TKey> : IQueryableRepository<TEntity, TKey>, IWriteableRepository<TEntity, TKey>
+    public interface IRepository<TEntity, TKey> 
+        : IQueryableRepository<TEntity, TKey>, IWriteableRepository<TEntity, TKey>
         where TEntity : AggregateRoot<TKey> 
         where TKey : struct;
 }
---
+++ src/BookStore.Domain/Persistence/IBooksRepository.cs
@@ -0,0 +1,7 @@
+﻿using BookStore.Domain.Models;
+using BookStore.Domain.Persistence.Common;
+
+namespace BookStore.Domain.Persistence
+{
+    public interface IBooksRepository : IRepository<Book, int>;
+}
